d75c015b465cf0426aba9d65a9af9f69830e80e3
Sandeep Bedadala <sandeepbedadala@Sandeeps-MacBook-Pro.local>
2016-06-09 20:25:56 -0400

    added about and contact pages

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var server = express();
 
-server.get('/', function(request,response){
+server.get('/', function(request, response){
   response.sendFile('public/html/index.html',{root:__dirname});
+});
+
+server.get('/about', function(request, response){
+  response.sendFile('public/html/about.html', {root: __dirname});
+});
+
+server.get('/contact', function(request, response){
+  response.sendFile('public/html/contact.html',{root: __dirname});
 });
 
 server.listen(8080);
